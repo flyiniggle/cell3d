f28909b7cdac210782bc1fe785fc62f1057f12ff
Dan Thompson <dan.thompson@thrivebio.com>
2022-10-24 12:20:36 -0600

    make loading pretty

--- client/app/c3d.js
+++ client/app/c3d.js
@@ -1,14 +1,4 @@
-// import * as THREE from 'https://unpkg.com/three@0.145.0/build/three.module.js';
-// import {
-//   OrbitControls
-// } from 'https://unpkg.com/three@0.145.0/examples/jsm/controls/OrbitControls';
-// import * as THREE from 'three';
-// import {
-//   OrbitControls
-// } from 'three/examples/jsm/controls/OrbitControls';
-import * as THREE from '/external/three/build/three.module.js'
-import { OrbitControls } from '/app/components/OrbitControls.js';
-import setVertexZPositions from '/app/lib/setVertexZPositions.js';
+import renderFullImage from '/app/lib/renderFullImage.js'
 
 
 export const loadImage = async () => {
@@ -19,53 +9,11 @@ export const loadImage = async () => {
 }
 
 export default async function main(viewer) {
-  const {
-    width,
-    height,
-    zMap
-  } = await loadImage();
+  const imageSpecs = await loadImage();
 
-  const geometry = new THREE.PlaneGeometry(width, height, width - 1, height - 1);
-  const scene = new THREE.Scene();
-  const texture = new THREE.TextureLoader().load('images/_Phi8Color.png');
-  const material = new THREE.MeshBasicMaterial({ map: texture });
-  const positions = geometry.attributes.position.array;
-  const newPositions = setVertexZPositions(zMap, positions);
+  document.getElementById('loader').style.display = 'none';
+  viewer.style.display = 'block';
+  renderFullImage(viewer, imageSpecs);
 
-  geometry.setAttribute( 'position', newPositions );
-  geometry.attributes.position.needsUpdate = true;
-
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
-
-  const renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setClearColor(0x555555 );
-  renderer.setSize(viewer.offsetWidth, viewer.offsetHeight);
-  viewer.appendChild(renderer.domElement);
-
-  const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 5000);
-
-
-  camera.position.z = 1000;
-
-  const controls = new OrbitControls(camera, renderer.domElement);
-
-  function animate() {
-
-    requestAnimationFrame( animate );
-
-    controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
-
-    renderer.render( scene, camera );
-
-  }
-  animate();
-
-  // mesh.rotation.x = -.2;
-  // mesh.rotation.y = -.2;
-  // mesh.rotation.z= .5;
-
-  // renderer.render(scene, camera);
-
-  console.log('rendered')
+  
 }
---
+++ client/app/lib/renderFullImage.js
@@ -0,0 +1,59 @@
+import * as THREE from '/external/three/build/three.module.js'
+import { OrbitControls } from '/app/components/OrbitControls.js';
+import setVertexZPositions from '/app/lib/setVertexZPositions.js';
+
+
+function renderFullImage(viewer, imageSpecs) {
+  const {
+    width,
+    height,
+    zMap
+  } = imageSpecs
+  const scene = new THREE.Scene();
+  const texture = new THREE.TextureLoader().load('images/_Phi8Color.png');
+  const material = new THREE.MeshPhongMaterial({ map: texture });
+
+  const geometry = new THREE.PlaneGeometry(width, height, width - 1, height - 1);
+  const positions = geometry.attributes.position.array;
+  const newPositions = setVertexZPositions(zMap, positions);
+  geometry.setAttribute( 'position', newPositions );
+  geometry.attributes.position.needsUpdate = true;
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const light = new THREE.AmbientLight( 0xffffff );
+  scene.add( light );
+
+  const renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setClearColor(0x555555 );
+  renderer.setSize(viewer.offsetWidth, viewer.offsetHeight);
+  viewer.appendChild(renderer.domElement);
+
+  const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 5000);
+  camera.position.x = 1000;
+  camera.position.y = -2000;
+  camera.position.z = 2000;
+
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.minPolarAngle = (Math.PI / 2);
+  controls.maxPolarAngle = Math.PI - .05;
+  controls.minAzimuthAngle = (Math.PI * -.25);
+  controls.maxAzimuthAngle = (Math.PI / 4);
+
+  function animate() {
+    console.log(controls.getPolarAngle())
+    requestAnimationFrame( animate );
+
+    controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
+    renderer.render( scene, camera );
+
+  }
+
+  animate();
+  console.log('rendered')
+}
+
+
+export default renderFullImage
